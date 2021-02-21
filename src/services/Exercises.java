cbf50365dfd4e979c036d272e1b0ac2e0d0909f8
rihardsaudze <audzerihards@inbox.lv>
2021-02-21 13:56:29 +0200

    Initial commit, finished exercises 0-8

---
+++ src/services/Exercises.java
@@ -0,0 +1,244 @@
+package services;
+import java.util.Random;
+import java.lang.Math;
+
+public class Exercises {
+	
+	public static void exer0() {
+		String[] names = {"Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda", "Aaron", "Kate"};
+		int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299, 343, 317, 265};
+		
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("["+names[i]+", "+times[i]+"]");
+		}
+	}
+	
+	public static double positionCalc(double gravity, double initialVelocity, double initialPosition, double fallingTime) {
+		return 0.5 * gravity * fallingTime*fallingTime + initialVelocity * fallingTime + initialPosition;
+	}
+	
+	public static int factorialForLoop(int N) {
+		if (N < 0) {
+			return 0;
+		}
+		int result = N;
+		for (N=N-1; N > 1; N--) {
+			result *= N;
+		}
+		return result;
+	}
+	
+	public static int factorialRecursive(int N) {
+		if (N < 0) {
+			return 0;
+		}
+	    if (N >= 1)
+	        return N * factorialRecursive(N - 1);
+	    else
+	        return 1;
+	}
+	
+	public static double[] generateArray(int N, double lower, double upper) {
+		double[] array = new double[N];
+		Random rand = new Random();
+		for (int i = 0; i < N; i++) {
+			array[i] = lower + rand.nextDouble() * (upper - lower);
+		}
+		return array;
+	}
+	
+	public static double getMean(double[] array) {
+		double result = 0;
+		for (int i = 0; i < array.length; i++) {
+			result += array[i];
+		}
+		return result/array.length;
+	}
+	
+	public static double getMin(double[] array) {
+		double min = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < min) {
+				min = array[i];
+			}
+		}
+		return min;
+	}
+	
+	public static double getMax(double[] array) {
+		double max = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > max) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
+	
+	public static double[] arraySort(double[] array) {
+	    int n = array.length; 
+	    for (int i = 0; i < n - 1; i++) { 
+	        for (int j = 0; j < n - i - 1; j++) { 
+	            if (array[j] > array[j + 1]) {  
+	                double temp = array[j]; 
+	                array[j] = array[j + 1]; 
+	                array[j + 1] = temp; 
+	            } 
+	        }
+	    }
+	    return array;
+	}
+	
+	public static double[][] generateMatrix(int N) {
+		double[][] matrix = new double[N][N];
+		Random rand = new Random();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = rand.nextDouble();
+			}
+		}
+		return matrix;
+	}
+	
+	public static double getProduct(double[][] matrix, int i, int j) {
+		double result = 0;
+		for (int k = 0; k < matrix[0].length; k++) {
+			result += matrix[i][k] * matrix[k][j];
+		}
+		return result;
+	}
+	
+	public static double[] coinFlip(int N) {
+		double[] result = new double[3];
+		Random rand = new Random();
+		double flip;
+		for (int i = 0; i < N; i++) {
+			flip = rand.nextDouble();
+			if (flip > 0.5) {
+				result[0] += 1;
+			} else {
+				result[1] += 1;
+			}
+		}
+		result[2] = result[0] / result[1];
+		return result;
+	}
+	
+	public static int[] rollDice(int N) {
+		int[] result = new int[6];
+		Random rand = new Random();
+		int flip;
+		for (int i = 0; i < N; i++) {
+			flip = rand.nextInt(6) + 1;
+			result[flip-1] += 1;
+		}
+		return result;
+	}	
+	
+	public static int roll2Dices() {
+		int tries = 0, dice1 = 0, dice2 = 0;
+		Random rand = new Random();
+		while(true) {
+			tries++;
+			dice1 = rand.nextInt(6) + 1;
+			dice2 = rand.nextInt(6) + 1;
+			if (dice1 + dice2 == 12) {
+				break;
+			}
+		}
+		return tries;
+	}
+	
+	public static String getTextFromBytes(byte[] array) {
+		return new String(array);
+	}
+	
+	public static String pascalsTriangle(int level) {
+		String result = new String();      
+		result += "[1, ";
+		for (int i = 1; i < level; i++) {
+			int res = factorialForLoop(level) / (factorialForLoop(i) * factorialForLoop(level - i));
+		    result += "" + res + ", ";
+		}        
+		result += "1]";
+        return result; 
+	}
+
+	public static double executeStringEquation(String inputEquation) {
+		double res = 0;
+		char c = 0;			//current char from string
+		boolean orderImportant = false;		//true when multiplication/division is required
+		int len = inputEquation.length(), oldindex, i, numLen = 0, numsIndex = 0, signIndex = 0;
+		int[] nums = new int[len/2+1];		//array of numbers from string
+		char[] signs = new char[len/2];		//array of signs from string
+		
+		for (i=0; i < len; i++) {			//parse string
+		    c = inputEquation.charAt(i);
+		    if (c < '0' || c > '9') {		//if current char not a number
+		        signs[signIndex] = c;		
+		        signIndex++;
+		        continue;
+		    }
+		    numLen = 1;
+		    oldindex = i;
+		    while(i < len-1) {				//read length of number
+		    	c = inputEquation.charAt(i+1);
+		    	if (c >= '0' && c <= '9') {
+		    		numLen++;
+		    		i++;
+		    	} else {
+		    		break;
+		    	}
+		    }
+		    i = oldindex;
+		    for (int j=numLen-1; j >= 0; j--) {		//calculate number from number string length
+		    	nums[numsIndex] += (int)((inputEquation.charAt(i) - '0') * Math.pow(10, j));
+		    	i++;
+		    }
+		    i--;
+		    numsIndex++;
+		}
+		//PARSING DONE, STARTING CALCULATIONS
+		
+		for (i=0; i < signs.length; i++) {		//check if equation contains multiplication/division
+		    if (signs[i] == '*' || signs[i] == '/') {
+		        orderImportant = true;
+		        break;
+		    }
+		}
+		
+		if(orderImportant==false) {				//CALCULATE
+    		res = nums[0];
+    		for (i=0; i < nums.length-1; i++) {
+    		    if (signs[i] == '+') {
+    		        res += nums[i+1];
+    		    } else {
+    		        res -= nums[i+1];
+    		    }
+    		}
+		} else {
+		    if (signs[i] == '*') {
+		        res = nums[i] * nums[i+1];
+		        nums[i] = 0;
+		        nums[i+1] = 0;
+		        signs[i] = '+';
+		    } else {
+		        res = nums[i] / nums[i+1];
+		        nums[i] = 0;
+		        nums[i+1] = 0;
+		        signs[i] = '+';
+		    }
+		    res += nums[0];
+		    for (i=0; i < nums.length-1; i++) {
+    		    if (signs[i] == '+') {
+    		        res += nums[i+1];
+    		    } else if (signs[i] == '-') {
+    		        res -= nums[i+1];
+    		    }
+    		}
+		}
+		
+		return res;
+	}
+	
+}
---
+++ src/services/HelloWorld.java
@@ -0,0 +1,51 @@
+package services;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {		
+		//Exercises.exer0();												//0
+		
+		//System.out.println(Exercises.positionCalc(-9.81, 0, 0, 10));		//1
+		
+		//System.out.println(Exercises.factorialForLoop(5));				//2.1
+		//System.out.println(Exercises.factorialRecursive(5));				//2.2
+		
+		//double[] genArray = Exercises.generateArray(5, 10, 20);			//3.1
+		//for (int i = 0; i < 5; i++) {
+		//	System.out.print(genArray[i]+" ");								
+		//}
+		//System.out.println("\nMin: "+Exercises.getMin(genArray));			//3.2
+		//System.out.println("Max: "+Exercises.getMax(genArray));			//3.3
+		//System.out.println("Mean: "+Exercises.getMean(genArray));			//3.4
+		//double[] sortedArray = Exercises.arraySort(genArray);				//3.5
+		//for (int i = 0; i < 5; i++) {
+		//	System.out.print(sortedArray[i]+" ");								
+		//}
+		
+		//double[][] genMatrix = Exercises.generateMatrix(5);				//4.1
+		//for (int i = 0; i < 5; i++) {
+		//	for (int j = 0; j < 5; j++) {
+		//		System.out.print(genMatrix[i][j]+" ");
+		//	}
+		//	System.out.println("");
+		//}
+		//double dotProduct = Exercises.getProduct(genMatrix, 2, 4);		//4.2
+		//System.out.println("Dot product (2;4): "+dotProduct);
+		
+		//double[] result = Exercises.coinFlip(100);						//5.1
+		//System.out.println("Heads: "+result[0]+"\nTails: "+result[1]+"\nRatio (h/t): "+result[2]);
+		//int[] result = Exercises.rollDice(100);							//5.2
+		//for (int i = 0; i < 6; i++) {
+		//	System.out.println((i+1)+" : "+result[i]);
+		//}
+		//int result = Exercises.roll2Dices();								//5.3
+		//System.out.println("Tries to get a 6&6 roll: "+result);
+		
+		//byte[] array = {72, 101, 108, 108, 111, 33, 32, 77, 121, 32, 115, 107, 105, 108, 108, 115, 32, 97, 114, 101,32, 103, 114, 101, 97, 116, 32, 97, 108, 114, 101, 97, 100, 121, 33};
+		//System.out.println(Exercises.getTextFromBytes(array));			//6
+		
+		//System.out.println(Exercises.pascalsTriangle(6));					//7
+		
+		System.out.println(Exercises.executeStringEquation("20+5*2+70"));	//8
+	}
+}
